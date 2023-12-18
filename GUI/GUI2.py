02bb2f2571174ead3032977d4d97640c89d95dda
Kingsavater <kingsavater5742@gmail.com>
2023-12-18 12:58:27 +0100

    getting started

--- GUI/GUI2.py
+++ GUI/GUI2.py
@@ -5,7 +5,7 @@ def button_func():
     #Get the content of the entry
     Entry_text=Entry.get()
     Label['text']=Entry_text
-#    Entry['disabled']='disabled'
+    Entry['state']='disabled'
 #    print(Label.configure())
 
 #creating the window
@@ -24,6 +24,16 @@ Entry.pack()
 #creating your button
 Button=ttk.Button(master=window, text='Enter',command=button_func)
 Button.pack()
+
+def rest_func():
+    Label['text']='Enter your info here'
+    Entry['state']='enabled'
+
+
+
+#exrerise button
+exerise_button=ttk.Button(master=window,text='exerise_button' , command=rest_func)
+exerise_button.pack()
  
 #runing
 window.mainloop()
